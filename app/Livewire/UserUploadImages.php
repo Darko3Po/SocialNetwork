bde85372080bd265573cd5d73a4f3c2ef66f4a41
Darko3Po <potajacdarko9@gmail.com>
2024-05-21 11:22:13 +0200

    Image upload + encode to webp & compression

--- app/Livewire/UserUploadImages.php
+++ app/Livewire/UserUploadImages.php
@@ -3,6 +3,10 @@
 namespace App\Livewire;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManager;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -10,18 +14,29 @@ class UserUploadImages extends Component
 {
     use WithFileUploads;
 
+    #[Validate([
+        'images.*' => [
+            'required', 'image', 'max:2048'
+        ]
+    ])]
+
     public array  $images = [];
     public function uploadImages(): JsonResponse
     {
-        $this->validate([
-            'images.*' => 'image|max:2048'
-        ]);
+        $this->validate();
+        foreach ($this->images as $image){
+           $fileName = rand(0,100).time();
 
-        die("RADIIIII 2 SLIKE");
+           $path = Storage::disk('public')->path('user_images')."/$fileName.webp";
+
+           ImageManager::gd()->read($image)->save($path);
+
+        }
 
         return response()->json([
-            'message' => 'success'
+            'mesage' => 'File upload was a success',
         ]);
+
     }
 
     public function render()
